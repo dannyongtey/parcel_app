465d15d07c766b23de50592a3879e2f8977bd9b5
Wei Long Tey <dannyongtey@gmail.com>
2017-05-27 13:33:02 +0000

    Enable case insensitive search

--- app/models/parcel.rb
+++ app/models/parcel.rb
@@ -4,6 +4,6 @@ class Parcel < ApplicationRecord
   validates(:phone, presence: true, length: {maximum: 15})
   default_scope -> {order(created_at: :desc)}
   def self.search(search_type, search_key)
-    Parcel.where("#{search_type} LIKE ?", "%#{search_key}%")
+    Parcel.where("#{search_type} ILIKE ?", "%#{search_key}%")
   end
 end
